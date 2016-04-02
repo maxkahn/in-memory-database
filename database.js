fd6746c06766c44088cc4c68caada7276ead6425
Max Kahn <kahn.max@gmail.com>
2016-04-02 16:39:18 -0700

    reads from file

--- database.js
+++ database.js
@@ -1,21 +1,11 @@
 var readline = require('readline');
 
-var main = function() {
-  if (arguments.length === 0) {
-    var rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
-      terminal: false
-    });
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+});
 
-    rl.on('line', function(line) {
-      console.log(line);
-    });
-  }
-  else {
-    
-  }
-
-};
-
-main();
+rl.on('line', function(line) {
+  console.log(line);
+});
