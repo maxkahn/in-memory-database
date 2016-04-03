6393747d32d2b1c889184a90b803997aae2bee0c
Max Kahn <kahn.max@gmail.com>
2016-04-02 17:07:11 -0700

    basic database commands

--- database.js
+++ database.js
@@ -1,5 +1,82 @@
 var readline = require('readline');
 
+var storage = {};
+var valueCounters = {};
+
+var set = function(name, value) {
+  if (!storage.hasOwnProperty(name)) {
+    storage[name] = value;
+    if (valueCounters.hasOwnProperty(value)) {
+      valueCounters[value]++;
+    }
+    else {
+      valueCounters[value] = 1;
+    }
+  }
+  else {
+    var oldValue = storage[name];
+    storage[name] = value;
+    valueCounters[oldValue]--;
+    if (valueCounters.hasOwnProperty(value)) {
+      valueCounters[value]++;
+    }
+    else {
+      valueCounters[value] = 1;
+    }
+  }
+};
+
+var get = function(name) {
+  if (storage.hasOwnProperty(name)) {
+    process.stdout.write(storage[name] + "\n");
+  }
+  else {
+    process.stdout.write("NULL\n");
+  }
+};
+
+var unset = function(name) {
+  if (storage.hasOwnProperty(name)) {
+    if (valueCounters[storage[name]] === 1) {
+      delete valueCounters[storage[name]];
+      delete storage[name];
+    }
+    else {
+      valueCounters[storage[name]]--;
+      delete storage[name];
+    }
+  }
+};
+
+var numequalto = function(value) {
+  if (valueCounters.hasOwnProperty(value)) {
+    process.stdout.write(valueCounters[value] + "\n");
+  }
+  else {
+    process.stdout.write("0\n");
+  }
+};
+
+var parseLine = function(str) {
+  var words = str.split(' ');
+  var funcWord = words[0];
+  words.shift();
+  var func;
+  if (funcWord === 'SET') {
+    func = set;
+  }
+  else if (funcWord === 'GET') {
+    func = get;
+  }
+  else if (funcWord === 'UNSET') {
+    func = unset;
+  }
+  else if (funcWord === 'NUMEQUALTO') {
+    func = numequalto;
+  }
+  func.apply(null, words);
+};
+
 var rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
@@ -7,5 +84,10 @@ var rl = readline.createInterface({
 });
 
 rl.on('line', function(line) {
-  console.log(line);
+  if (line === 'END') {
+    rl.close();
+  }
+  else {
+    parseLine(line);
+  }
 });
